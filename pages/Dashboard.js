168674240309884960d19948bd224ef3f183b7d5
ephraim888sun <ephraim888sun@gmail.com>
2022-02-04 16:38:00 -0600

    new

---
+++ pages/Dashboard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Header from '../components/Header'
+import styled from 'styled-components';
+import Main from '../components/Main';
+
+const Dashboard = ({ address }) => {
+    return (
+        <Wrapper>
+            <MainContainer>
+                <Header />
+                <Main />
+            </MainContainer>
+        </Wrapper>
+    )
+}
+
+export default Dashboard
+
+const Wrapper = styled.div`
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+    background-color; #0a0b0d;
+    color: white;
+`
+
+const MainContainer = styled.div`
+flex: 1;
+`
